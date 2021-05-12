babe9a6f3cc0f686c0f16c4e65dc5aacb60a8e5d
Quinn Wilson <qtwilson10@gmail.com>
2021-05-12 19:31:39 -0400

    initial commit

--- Bouncer-2/RestaurantScene/RestaurantViewController.swift
+++ Bouncer-2/RestaurantScene/RestaurantViewController.swift
@@ -1,19 +1,19 @@
 //
-//  ViewController.swift
+//  RestaurantViewController.swift
 //  Bouncer-2
 //
-//  Created by Quinn Wilson on 5/11/21.
+//  Created by Quinn Wilson on 5/12/21.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class RestaurantViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+ 
 
 }
-
